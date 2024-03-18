dd704af92cd40fdaed75547257811327a1e27ab7
Mustang <nubchenko@gmail.com>
2024-03-18 23:41:17 +0200

    added section about

--- dior/js/main.js
+++ dior/js/main.js
@@ -1,9 +1,19 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.top__slider', {
   effect: 'fade',
   autoplay: {
     delay: 3000,
     disableOnInteraction: false
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
